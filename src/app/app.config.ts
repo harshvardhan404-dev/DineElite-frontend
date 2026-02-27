d222985d5519de946618a9a24e375d711beb3b76
harshvardhan404-dev <harshvardhansonawane2@gmail.com>
2026-02-28 02:59:01 +0530

    Fix: Add withCredentials interceptor for cross-domain session cookies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([credentialsInterceptor]))]
 };
